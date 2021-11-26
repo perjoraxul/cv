9b0b38a0e03f1446593aa1ba4100590a434e3b14
Boby Cross <lollolix@yahoo.com>
2021-11-26 10:53:14 +0200

    3rd commit

---
+++ 2day/years left/main.py
@@ -0,0 +1,12 @@
+age = input("What is your current age?")
+
+
+age_int = int(age)
+years_left = 90 - age_int
+
+days = years_left * 365
+weeks = years_left * 52
+months = years_left * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
